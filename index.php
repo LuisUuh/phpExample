b4240b947aab341aae3b3fc2aaab6d2b06ce347d
LuisUuh <angel.uuh.9406@gmail.com>
2019-07-07 13:44:14 -0500

    Commit initial
    UuhL

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Store</title>
+    <!-- estilosNormales -->
+    <link rel='stylesheet' href='css/style.css'>
+    <!-- linkBootstrap -->
+    <link rel='stylesheet' href='css/bootstrap.min.css'>
+    <!-- linkGoogleFonts -->
+
+    <!-- linkFontAwesome -->
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' integrity='sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf' crossorigin='anonymous'>
+</head>
+<body>
+    <header>
+    </header>
+    <section>
+    </section>
+    <footer>
+    </footer>
+    <!-- bootstrap -->
+    <script src='js/jquery-3.3.1.min.js'></script>
+    <script src='js/bootstrap.min.js'></script>
+</body>
+</html>
